cce3fb26b7b59107df47fac7b6e12c0283294691
Adaa <adaamgbede@gmail.com>
2020-03-31 16:53:11 +0100

    calculate interest

--- jstask2.js
+++ jstask2.js
@@ -6,23 +6,28 @@ let principalTimeArr = [
 ]
 
 let interestData = [];
-function calculateInterest(principal, rate, time) {
-    let interest = (principal * rate * time)/100
-    interestData.push({'principal': principal, 'rate': rate, 'time': time, 'interest': interest});
+let rate = '';
+
+function calculateInterest(arr) {
+    arr.forEach(element => {
+        if(element.principal >= 2500 && (element.time > 1 && element.time <= 3)) {
+            rate = 3;
+        } else if(element.principal >= 2500 && element.time >= 3) {
+            rate = 4;
+        } else if (element.principal < 2500 || element.time <= 1) {
+            rate = 2;
+        } else {
+            rate = 1;  
+        }
+
+        let interest = (element.principal * rate * element.time)/100
+        interestData.push(
+            {'principal': element.principal, 'rate': rate, 'time': element.time, 'interest': interest}
+        );
+    });
+    
     console.log(interestData)
     return interestData;
 }
 
-let rate = '';
-principalTimeArr.forEach(element => {
-    if(element.principal >= 2500 && (element.time > 1 && element.time <= 3)) {
-        rate = 3;
-    } else if(element.principal >= 2500 && element.time >= 3) {
-        rate = 4;
-    } else if (element.principal < 2500 || element.time <= 1) {
-        rate = 2;
-    } else {
-        rate = 1;  
-    }
-    calculateInterest(element.principal, rate, element.time)
-});
+calculateInterest(principalTimeArr)
